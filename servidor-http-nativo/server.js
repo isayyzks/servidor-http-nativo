dcff9bbc9e4fca4f4ae890e0890ad2089f0c3e12
Isa✯ <isabellealves4567@gmail.com>
2026-08-13 18:49:29 +0000

    edit: Context-type alterado

--- servidor-http-nativo/server.js
+++ servidor-http-nativo/server.js
@@ -6,9 +6,9 @@ const server = http.createServer((req, res) => {
     
 
     res.statusCode = 201;
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    res.setHeader('Content-Type', 'application/json; charset=utf-8');
 
-    res.end("Recurso criado");
+    res.end(JSON.stringify({ status: "ok" }));
 
 });
 
