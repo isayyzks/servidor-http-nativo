6c7031ba9bc727b09418fc0132b4f5b03a294b4e
Isa✯ <isabellealves4567@gmail.com>
2026-08-18 03:16:44 +0000

    edit: exercício 4

--- servidor-http-nativo/server.js
+++ servidor-http-nativo/server.js
@@ -1,48 +1,51 @@
-import http from 'node:http'
-import { URL } from 'node:url'
+import http from 'node:http' 
+import { URL } from 'node:url' 
+ 
+const porta = 3000 
+ 
+const produtos = [ 
+    {id: 1, nome: "Sabonete"}, 
+    {id: 2, nome: "Volante LogiTech G923"}, 
+    {id: 3, nome: "Sabão em Pó"}, 
+    {id: 4, nome: "Pelúcia do Sonic"}, 
+] 
+ 
+const server = http.createServer((req, res) => { 
 
-const porta = 3000
+    console.log(req.method, req.url)
 
-const produtos = [
-    {id: 1, nome: "Sabonete"},
-    {id: 2, nome: "Volante LogiTech G923"},
-    {id: 3, nome: "Sabão em Pó"},
-    {id: 4, nome: "Pelúcia do Sonic"},
-]
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'application/json; charset=utf-8')
-
-    if (req.method == "GET" && req.url == "/contato") {
-        return res.end(JSON.stringify({data:
-            {numero_telefone: "67 99999 9999",
-                endereco: "Rua da Alegria, 99, Centro"}}));
-    } else{
-        res.statusCode = 404;
-        res.end('Rota não encontrada')
-     } 
-
-    if (req.method == "GET" && req.url == "/produtos") {
-        return res.end(JSON.stringify(produtos));
-
-    } else{
-        res.statusCode = 404;
-        res.end('Rota não encontrada')
-     } 
-
-    if (req.method == "GET" && req.url == "/status") {
-        return res.end(JSON.stringify({"status": "ok" }));
-        
-     } else{
-        res.statusCode = 404;
-        res.end('Rota não encontrada')
-     } 
-    
-
-    res.end(JSON.stringify({data: "Página Inicial"}))
-})
-
-server.listen(porta, () => {
+    res.statusCode = 200 
+    res.setHeader('Content-Type', 'application/json; charset=utf-8') 
+ 
+    if (req.method == "GET" && req.url == "/contato") { 
+        return res.end(JSON.stringify({data: 
+            {numero_telefone: "67 99999 9999", 
+                endereco: "Rua da Alegria, 99, Centro"}})); 
+    } else{ 
+        res.statusCode = 404; 
+        res.end('Rota não encontrada') 
+     }  
+ 
+    if (req.method == "GET" && req.url == "/produtos") { 
+        return res.end(JSON.stringify(produtos)); 
+ 
+    } else{ 
+        res.statusCode = 404; 
+        res.end('Rota não encontrada') 
+     }  
+ 
+    if (req.method == "GET" && req.url == "/status") { 
+        return res.end(JSON.stringify({"status": "ok" })); 
+         
+     } else{ 
+        res.statusCode = 404; 
+        res.end('Rota não encontrada') 
+     }  
+     
+ 
+    res.end(JSON.stringify({data: "Página Inicial"})) 
+}) 
+ 
+server.listen(porta, () => { 
     console.log(`Servidor ouvindo na porta ${porta}`)
 });
